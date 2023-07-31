49b9f9bc31b1a42743f43c2c3eccf3b6d6e7ebd9
Fathimathu Farhana <fathimathufarhana4444@gmail.com>
2023-07-31 17:12:14 +0530

    Commit1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Signup from "./Signup";
+import Login from "./Login";
+
+
+
+
+function App(){
+    return(
+       <div>
+
+<Signup/>
+    
+<Login/>
+       </div>
+
+    )
+}
+
+
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,44 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-// import { Router } from 'react-router-dom';
-
-import Layout from './Layout';
-import Application from './facebook/Application';
-import Password from './facebook/Password';
-import Security from './facebook/Security';
-import Account from './facebook/Account';
-import Notification from './facebook/Notification';
-// import Fetch from './Fetch';
-
+// import App from "./App";
+// import Hook from './Hook';
+import Form from './Form';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Layout />}>
-                    <Route path='/application' element={<Application />}></Route>
-                    <Route path='/password' element={<Password />}></Route>
-
-                    <Route path='/Security' element={<Security />}></Route>
-
-                    <Route path='/notification' element={<Notification />}></Route>
-
-                    <Route path='' element={<Account />}></Route>
-
-
-            </Route>
-        </Routes>
-    </BrowserRouter >
-
-    </>
-
-    
-
-
+    // <App/>
+    // <Hook/>
+    <Form/>
 );
-
 
