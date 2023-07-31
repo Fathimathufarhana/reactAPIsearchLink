3a5d5ce5bb06e62431de71ca33338763cfd50caa
Fathimathu Farhana <fathimathufarhana4444@gmail.com>
2023-07-31 16:36:32 +0530

    Commit1

--- src/index.js
+++ src/index.js
@@ -10,32 +10,32 @@ import Password from './facebook/Password';
 import Security from './facebook/Security';
 import Account from './facebook/Account';
 import Notification from './facebook/Notification';
-import Fetch from './Fetch';
+// import Fetch from './Fetch';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    // <>
-    //     <BrowserRouter>
-    //         <Routes>
-    //             <Route path='/' element={<Layout />}>
-    //                 <Route path='/application' element={<Application />}></Route>
-    //                 <Route path='/password' element={<Password />}></Route>
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route path='/application' element={<Application />}></Route>
+                    <Route path='/password' element={<Password />}></Route>
 
-    //                 <Route path='/Security' element={<Security />}></Route>
+                    <Route path='/Security' element={<Security />}></Route>
 
-    //                 <Route path='/notification' element={<Notification />}></Route>
+                    <Route path='/notification' element={<Notification />}></Route>
 
-    //                 <Route path='' element={<Account />}></Route>
+                    <Route path='' element={<Account />}></Route>
 
 
-    //         </Route>
-    //     </Routes>
-    // </BrowserRouter >
+            </Route>
+        </Routes>
+    </BrowserRouter >
 
-    // </>
-<Fetch/>
+    </>
+
     
 
 
