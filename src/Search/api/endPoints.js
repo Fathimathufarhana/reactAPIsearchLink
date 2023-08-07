9a8d86545e01f3e1326b4bf0e21d33a213bf0ece
Fathimathu Farhana <fathimathufarhana4444@gmail.com>
2023-08-07 12:00:10 +0530

    commit1

---
+++ src/Search/api/endPoints.js
@@ -0,0 +1,4 @@
+export const categories = 'categories.php';
+export const random = 'random.php';
+// export const search_end = `search.php?s=Arrabiata`
+export const area = `filter.php?a=Canadian`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Home from './Search/Home';
+// import Home from './Meal/Home';
+import "./Search/style.css"
+// import "./Meal/style.css";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
-import Form from './Form';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <Form/>
+    
+ 
+    <>
+    <Home/>
+    <ToastContainer />
+    </>
+ 
 );
 
